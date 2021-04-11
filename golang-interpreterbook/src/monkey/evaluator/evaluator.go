4a3145dc713cd98497c7af3014d4fbaf8e90856f
Mizue Ando <mzeand33@gmail.com>
2021-04-11 13:06:28 +0900

    add: Chapter3.5.5

--- golang-interpreterbook/src/monkey/evaluator/evaluator.go
+++ golang-interpreterbook/src/monkey/evaluator/evaluator.go
@@ -98,16 +98,19 @@ func evalInfixExpression(
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
+	case operator == "==":
+		return nativeBoolToBooleanObject(left == right)
+	case operator == "!=":
+		return nativeBoolToBooleanObject(left != right)
 	default:
 		return NULL
 	}
 }
 
-
 func evalIntegerInfixExpression(
 	operator string,
 	left, right object.Object,
-) object.Object {
+	) object.Object {
 	leftVal := left.(*object.Integer).Value
 	rightVal := right.(*object.Integer).Value
 
@@ -120,14 +123,14 @@ func evalIntegerInfixExpression(
 		return &object.Integer{Value: leftVal * rightVal}
 	case "/":
 		return &object.Integer{Value: leftVal / rightVal}
-	//case "<":
-	//	return nativeBoolToBooleanObject(leftVal < rightVal)
-	//case ">":
-	//	return nativeBoolToBooleanObject(leftVal > rightVal)
-	//case "==":
-	//	return nativeBoolToBooleanObject(leftVal == rightVal)
-	//case "!=":
-	//	return nativeBoolToBooleanObject(leftVal != rightVal)
+	case "<":
+		return nativeBoolToBooleanObject(leftVal < rightVal)
+	case ">":
+		return nativeBoolToBooleanObject(leftVal > rightVal)
+	case "==":
+		return nativeBoolToBooleanObject(leftVal == rightVal)
+	case "!=":
+		return nativeBoolToBooleanObject(leftVal != rightVal)
 	default:
 		return NULL
 	}
