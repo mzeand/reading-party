e1aa7fc70394b44a09a01d3127fb8cd95c01b678
Mizue Ando <mzeand33@gmail.com>
2021-05-16 11:01:13 +0900

    add comments

--- golang-interpreterbook/src/monkey/object/environment.go
+++ golang-interpreterbook/src/monkey/object/environment.go
@@ -1,19 +1,23 @@
 package object
 
-func NewEnvironment()  *Environment {
+// NewEnvironment return Environment
+func NewEnvironment() *Environment {
 	s := make(map[string]Object)
 	return &Environment{store: s}
 }
 
-type Environment struct{
+//
+type Environment struct {
 	store map[string]Object
 }
 
+// Get return the environment object and return status
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
 	return obj, ok
 }
 
+// Set set map of a environment
 func (e *Environment) Set(name string, val Object) Object {
 	e.store[name] = val
 	return val
